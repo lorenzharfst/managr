a2d72306184f93948148dfc3b5de1ce431ffaa30
Lorenz Harfst <lorenzharfst@gmail.com>
2025-12-11 20:35:53 +0100

    Initialise with maven

---
+++ src/main/java/dev/lorenzharfst/managr/App.java
@@ -0,0 +1,13 @@
+package dev.lorenzharfst.managr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
