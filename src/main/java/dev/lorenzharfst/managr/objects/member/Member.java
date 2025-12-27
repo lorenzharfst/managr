8db26bebeddf0a07284f4ae3a8cf4053d217973c
Lorenz Harfst <lorenzharfst@gmail.com>
2025-12-27 16:36:36 +0100

    Remove site folder (created by accident with mvn)

--- src/main/java/dev/lorenzharfst/managr/objects/member/Member.java
+++ src/main/java/dev/lorenzharfst/managr/objects/member/Member.java
@@ -1,3 +1,13 @@
 package dev.lorenzharfst.managr.objects.member;
 
-public class Member { }
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+}
