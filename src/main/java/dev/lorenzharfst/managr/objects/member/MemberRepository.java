47b1ddfd067dd03d640a95ba5205ad301907abaf
Lorenz Harfst <lorenzharfst@gmail.com>
2026-01-23 00:23:26 +0100

    Add join club logic to MemberService

--- src/main/java/dev/lorenzharfst/managr/objects/member/MemberRepository.java
+++ src/main/java/dev/lorenzharfst/managr/objects/member/MemberRepository.java
@@ -1,5 +1,10 @@
 package dev.lorenzharfst.managr.objects.member;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> { }
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    public Optional<Member> findByUsername(String username);
+}
--- src/main/java/dev/lorenzharfst/managr/objects/member/MemberService.java
+++ src/main/java/dev/lorenzharfst/managr/objects/member/MemberService.java
@@ -1,7 +1,33 @@
 package dev.lorenzharfst.managr.objects.member;
 
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import dev.lorenzharfst.managr.objects.club.Club;
+import dev.lorenzharfst.managr.objects.club.ClubRepository;
 
 @Service
 public class MemberService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    ClubRepository clubRepository;
+
+    /**
+     * Given a member's login username, joins them to a club. Only the authenticated user can perform this action.
+     * @param memberUsername The actual log in username, which is unique.
+     * @param clubId
+     */
+    public void joinClub(String memberUsername, long clubId) throws NoSuchElementException {
+        // Will return null if it didn't find that club
+        Club club = clubRepository.findById(clubId).orElseThrow(NoSuchElementException::new);
+        Member member = memberRepository.findByUsername(memberUsername).orElseThrow(NoSuchElementException::new);
+
+        club.getMembers().add(member);
+        clubRepository.save(club);
+    }
 }
